b4288a2ace35fa9a8a232642a937b3b88b0c25d1
Your <your-email@domain.com>
2025-06-15 15:25:52 +0200

    Update OnchainKitProvider.tsx

--- src/components/OnchainKitProvider.tsx
+++ src/components/OnchainKitProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
 import { WagmiProvider, createConfig } from 'wagmi';
 import { http } from 'viem';
